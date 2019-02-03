6198d3578a53b2bcef70e2d6c1c62f7328004ff5
JariESavolainen <30497791+JariESavolainen@users.noreply.github.com>
2019-02-03 16:40:10 +0200

    8

--- MyFirstWebApp_8/src/requestmapper.cpp
+++ MyFirstWebApp_8/src/requestmapper.cpp
@@ -1,23 +1,24 @@
 #include "requestmapper.h"
 #include "httpsession.h"
 
-HttpSessionStore* RequestMapper::sessionStore=0;
-StaticFileController* RequestMapper::staticFileController=0;
-TemplateCache* RequestMapper::templateCache=0;
-Logger* RequestMapper::logger=0;
+HttpSessionStore* RequestMapper::sessionStore = nullptr;
+StaticFileController* RequestMapper::staticFileController = nullptr;
+TemplateCache* RequestMapper::templateCache = nullptr;
+Logger* RequestMapper::logger = nullptr;
 
 RequestMapper::RequestMapper(QObject* parent)
-    : HttpRequestHandler(parent) {
+    : HttpRequestHandler(parent)
+{
     // empty
 }
 
-void RequestMapper::service(HttpRequest& request, HttpResponse& response) {
-    QByteArray path=request.getPath();
-    qDebug("RequestMapper: path=%s",path.data());
-    HttpSession session=sessionStore->getSession(request,response,false);
-    QString username=session.get("username").toString();
-    logger->set("currentUser",username);
-
+void RequestMapper::service(HttpRequest& request, HttpResponse& response)
+{
+    QByteArray path = request.getPath();
+    qDebug("RequestMapper: path=%s", path.data());
+    HttpSession session = sessionStore->getSession(request, response, false);
+    QString username = session.get("username").toString();
+    logger->set("currentUser", username);
 
     /*
     QByteArray sessionId=sessionStore->getSessionId(request,response);
@@ -28,30 +29,23 @@ void RequestMapper::service(HttpRequest& request, HttpResponse& response) {
     }
     */
 
-    if (path=="/" || path=="/hello") {
+    if (path == "/" || path == "/hello") {
         helloWorldController.service(request, response);
-    }
-    else if (path=="/list") {
+    } else if (path == "/list") {
         listDataController.service(request, response);
-    }
-    else if (path=="/login") {
+    } else if (path == "/login") {
         loginController.service(request, response);
-    }
-    else if (path=="/cookie") {
+    } else if (path == "/cookie") {
         cookieTestController.service(request, response);
-    }
-    else if (path=="/list2") {
+    } else if (path == "/list2") {
         dataTemplateController.service(request, response);
-    }
-    else if (path.startsWith("/files")) {
-        staticFileController->service(request,response);
-    }
-    else {
-        response.setStatus(404,"Not found");
+    } else if (path.startsWith("/files")) {
+        staticFileController->service(request, response);
+    } else {
+        response.setStatus(404, "Not found");
         response.write("The URL is wrong, no such document.");
     }
 
     qDebug("RequestMapper: finished request");
-    logger->clear(true,true);
+    logger->clear(true, true);
 }
-
